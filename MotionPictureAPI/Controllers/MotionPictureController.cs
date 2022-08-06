95eb7c95531fa588388f274c40d036ebeb5f65a0
Nichole Zacherl <nlz16214@gmail.com>
2022-08-06 17:29:38 -0400

    added API project

---
+++ MotionPictureAPI/Controllers/MotionPictureController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotionPictureAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MotionPictureController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MotionPictureController> _logger;
+
+        public MotionPictureController(ILogger<MotionPictureController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<MotionPicture> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new MotionPicture
+            {
+                ID = index,
+                Name = Summaries[rng.Next(Summaries.Length)],
+                ReleaseYear = DateTime.Now.AddDays(index).Year,
+                Description = Summaries[rng.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MotionPictureAPI/MotionPicture.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MotionPictureAPI
+{
+    public class MotionPicture
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int ReleaseYear { get; set; }
+    }
+}
